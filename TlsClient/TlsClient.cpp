fe7c50f5b375f50c6cf8a2c065ab80cc28d4c9b3
Ant <6o4kin@gmail.com>
2025-06-30 12:50:59 +0300

    Добавлена обработка аргументов командной строки

--- TlsClient/TlsClient.cpp
+++ TlsClient/TlsClient.cpp
@@ -2,11 +2,25 @@
 //
 
 #include "TlsClient.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: TlsClient <client_key.pem> <client_cert.pem> <ca_cert.pem>\n";
+        return 1;
+    }
+
+    std::string clientKeyFile = argv[1];
+    std::string clientCertFile = argv[2];
+    std::string caCertFile = argv[3];
+
+    std::cout << "Key: " << clientKeyFile << std::endl;
+    std::cout << "Cert: " << clientCertFile << std::endl;
+    std::cout << "CA: " << caCertFile << std::endl;
+
+    return 0;
 }
+
