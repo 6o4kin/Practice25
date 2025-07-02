31df317598d71fc10392e1b15c7501289e4bb361
Ant <6o4kin@gmail.com>
2025-07-02 12:40:22 +0300

    Добавлена инициализация SSL и загрузка сертификатов

--- TlsClient/TlsClient.cpp
+++ TlsClient/TlsClient.cpp
@@ -4,6 +4,7 @@
 #include "TlsClient.h"
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <Poco/Net/SSLManager.h>
 #include <Poco/Net/Context.h>
 #include <Poco/Net/HTTPSClientSession.h>
@@ -12,7 +13,16 @@
 #include <Poco/Net/AcceptCertificateHandler.h>
 #include <Poco/Net/InvalidCertificateHandler.h>
 #include <Poco/SharedPtr.h>
+#include <Poco/Net/PrivateKeyPassphraseHandler.h>
+#include <Poco/Net/X509Certificate.h>
+#include <Poco/Net/SecureStreamSocket.h>
+#include <Poco/Net/DialogSocket.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/Exception.h>
+
 using namespace std;
+using namespace Poco::Net;
+using namespace Poco;
 
 int main(int argc, char* argv[]) {
     if (argc != 4) {
@@ -27,6 +37,39 @@ int main(int argc, char* argv[]) {
     std::cout << "Key: " << clientKeyFile << std::endl;
     std::cout << "Cert: " << clientCertFile << std::endl;
     std::cout << "CA: " << caCertFile << std::endl;
+
+    try {
+        // Инициализация SSL
+        initializeSSL();
+
+        // Обработчик сертификатов (принимает любой)
+        SharedPtr<InvalidCertificateHandler> pCertHandler =
+            new AcceptCertificateHandler(false);
+
+        // TLS-контекст с нашими файлами
+        Context::Ptr context = new Context(
+            Context::CLIENT_USE,
+            clientKeyFile,
+            clientCertFile,
+            caCertFile,
+            Context::VERIFY_RELAXED,
+            9,
+            true
+        );
+
+        // Установка глобального SSL менеджера
+        SSLManager::instance().initializeClient(
+            nullptr,      // можно позже добавить обработчик пароля
+            pCertHandler,
+            context
+        );
+
+        std::cout << "TLS-контекст успешно инициализирован\n";
+    }
+    catch (const Exception& ex) {
+        std::cerr << "Ошибка TLS: " << ex.displayText() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
